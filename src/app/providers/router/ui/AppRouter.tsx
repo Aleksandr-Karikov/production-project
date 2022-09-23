ce0f86ee766c606e6bf4104dc68c216237e384b4
Aleksandr <a.karikov@powwwer.io>
2022-09-23 13:02:09 +0700

    add loader and pageLoader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,25 +1,26 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
 const AppRouter = () => (
-    <Suspense fallback={<div>loading</div>}>
-        <Routes>
-            {
-                Object.values(routeConfig).map(({ element, path }) => (
-                    <Route
-                        path={path}
-                        key={path}
-                        element={(
+    <Routes>
+        {
+            Object.values(routeConfig).map(({ element, path }) => (
+                <Route
+                    path={path}
+                    key={path}
+                    element={(
+                        <Suspense fallback={<PageLoader />}>
                             <div className="page-wrapper">
                                 {element}
                             </div>
-                        )}
-                    />
-                ))
-            }
-        </Routes>
-    </Suspense>
+                        </Suspense>
+                    )}
+                />
+            ))
+        }
+    </Routes>
 );
 
 export default AppRouter;
