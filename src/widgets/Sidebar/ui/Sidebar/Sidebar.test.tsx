34cd0bccab214e5403fb8f069fcd6684d759838e
Aleksandr <a.karikov@powwwer.io>
2022-10-04 12:47:36 +0700

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,17 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
-import {
-    renderWithTranslations,
-} from '../../../../shared/lib/tests/renderWithTranslations/renderWithTranslations';
 
 describe('Sidebar', () => {
     test('render', () => {
-        renderWithTranslations(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('test toggle', () => {
-        renderWithTranslations(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
