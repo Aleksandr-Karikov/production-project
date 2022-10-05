21cff03112101cc452e0858b6f86d5ad4f640413
Aleksandr <a.karikov@powwwer.io>
2022-10-05 16:12:40 +0700

    add redux store add counter

---
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -0,0 +1,29 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { counterActions, counterReducer } from './counterSlice';
+import { CounterSchema } from '../types/counterSchema';
+
+describe('counterSlice.test', () => {
+    test('counter decrement', () => {
+        const state: CounterSchema = {
+            value: 10,
+        };
+        expect(
+            counterReducer(state, counterActions.decrement()),
+        ).toEqual({ value: 9 });
+    });
+    test('counter increment', () => {
+        const state: CounterSchema = {
+            value: 10,
+        };
+        expect(
+            counterReducer(state, counterActions.increment()),
+        ).toEqual({ value: 11 });
+    });
+
+    test('empty', () => {
+        expect(
+            counterReducer(undefined, counterActions.increment()),
+        ).toEqual({ value: 1 });
+    });
+});
